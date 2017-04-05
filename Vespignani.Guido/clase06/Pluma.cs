538d6f4dc3393ace62ba8a72fa22a07ec2356d15
guidoves <guido.vespignani@gmail.com>
2017-04-04 22:13:49 -0300

    clase 6

---
+++ Vespignani.Guido/clase06/Pluma.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace clase06
+{
+    public class Pluma
+    {
+        private String _marca;
+        private Tinta _tinta;
+        private int _cantidad;
+
+        public Pluma(String marca, Tinta tinta, int cantidad)
+        {
+            this._marca = marca;
+            this._tinta = tinta;
+            this._cantidad = cantidad;
+        }
+        public Pluma(String marca, Tinta tinta):this()
+        {
+            this._marca = marca;
+            this._tinta = tinta; 
+        }
+        public Pluma()
+        {
+            this._marca = "Sin marca";
+            this._tinta = null;
+            this._cantidad = 1;
+        }
+
+        private String Mostrar()
+        {
+            return (String)this._tinta;
+        }
+
+        public static implicit operator String(Pluma a)
+        {
+            return a;
+        }
+
+        public static Boolean operator ==(Pluma a, Tinta b)
+        {
+            return a._tinta == b ? true : false;
+        }
+
+        public static Boolean operator !=(Pluma a, Tinta b)
+        {
+            return !(a == b);
+        }
+
+        public static Pluma operator +(Pluma a, Tinta b)
+        {
+            if (a == b)
+                a._cantidad++;
+            return a;
+        }
+        public static Pluma operator -(Pluma a, Tinta b)
+        {
+            if (a == b)
+                if (a._cantidad != 0)
+                    a._cantidad--;
+                else
+                    a._tinta = null;
+            return a;
+        }
+    }
+}
---
+++ Vespignani.Guido/clase06/Tinta.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase06
+{
+    public class Tinta
+    {
+        private ConsoleColor _color;
+        private ETipo _tipo;
+
+        public Tinta(ConsoleColor color, ETipo tipo)
+        {
+            this._color = color;
+            this._tipo = tipo;
+        }
+        public Tinta(ConsoleColor color)
+            
+        {
+            this._color = color;
+            this._tipo = ETipo.COMUN;
+        }
+        public Tinta(ETipo tipo)
+        {
+            this._tipo = tipo;
+            this._color = ConsoleColor.Black;
+        }
+        public Tinta()
+        {
+            this._tipo = ETipo.COMUN;
+            this._color = ConsoleColor.Black;
+        }
+
+        private String Mostrar()
+        {
+            return this._color.ToString() + this._tipo.ToString();
+        }
+
+        public static String Mostrar(Tinta miTinta)
+        {
+            return miTinta.Mostrar();
+        }
+
+        public static Boolean operator ==(Tinta a, Tinta b)
+        {
+            return (a._color == b._color && a._tipo == b._tipo) ? true : false;
+        }
+
+        public static Boolean operator !=(Tinta a, Tinta b)
+        {
+            return !(a == b);
+        }
+
+        public static explicit operator String(Tinta a)
+        {
+            return a.ToString();
+        }
+    }
+
+    public enum ETipo {COMUN,CHINA,CONBRILLITO }
+}
