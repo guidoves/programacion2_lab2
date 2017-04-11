093fd13f3994e97770fc564a51107b14bf320dcc
guidoves <guido.vespignani@gmail.com>
2017-04-10 22:12:29 -0300

    clase 7

---
+++ Vespignani.Guido/Clase7/Paleta.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase7
+{
+    public class Paleta
+    {
+        private Tempera[] _temperas;
+        private int _cantMaxColores;
+
+        private Paleta():this(5)
+        {
+        }
+        public Paleta(int cantidad):this()
+        {
+            this._temperas = new Tempera[cantidad];
+            this._cantMaxColores = cantidad;
+
+        }
+
+        private String mostrar()
+        {
+            String paleta = this._cantMaxColores.ToString();
+
+            foreach(Tempera i in this._temperas)
+            {
+                paleta = paleta + " " + Tempera.Mostrar(i);
+            }
+            return paleta;
+        }
+
+        public static implicit operator Paleta(int a)
+        {
+            Paleta p = new Paleta(a);
+            return p;
+        }
+        public static explicit operator String(Paleta a)
+        {
+            return a.mostrar();
+        }
+        public static Boolean operator ==(Paleta a, Tempera b)
+        {
+            foreach (Tempera i in a._temperas)
+            {
+                if (b == i)
+                    return true;
+            }
+            return false;
+        }
+        public static Boolean operator !=(Paleta a, Tempera b)
+        {
+            return !(a == b);
+        }
+        public static Paleta operator +(Paleta a, Tempera b)
+        {
+            foreach (Tempera i in a._temperas)
+            {
+                if (b == i)
+                    a._
+                return a;
+            }
+            foreach (Tempera i in a._temperas)
+            {
+                if (i == null)
+                {
+                    a._temperas[i] = b;
+                    break;
+                }
+            }
+            return a;
+        }
+        public static Paleta operator -(Paleta a, Tempera b)
+        {
+            foreach (Tempera i in a._temperas)
+            {
+                if (b == i && a._cantMaxColores <= 0)
+                {
+                    a._temperas[i] = null;
+                    break;
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ Vespignani.Guido/Clase7/Tempera.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase7
+{
+    public class Tempera
+    {
+        #region Variables de instancia
+
+        private ConsoleColor _color;
+        private String _marca;
+        private int _cantidad;
+
+        #endregion
+
+        #region Constructor
+
+        public Tempera(ConsoleColor color, String marca, int cantidad)
+        {
+            this._color = color;
+            this._marca = marca;
+            this._cantidad = cantidad;
+        }
+
+        #endregion
+
+
+        public static String Mostrar(Tempera a)
+        {
+            return a.Mostrar();
+        }
+
+        private String Mostrar()
+        {
+            return this._marca + " " + this._color + " " + this._cantidad;
+        }
+
+        public static Boolean operator ==(Tempera a, Tempera b)
+        {
+            if (a._color == b._color && a._marca == b._marca)
+                return true;
+            return false;
+        }
+        public static Boolean operator !=(Tempera a, Tempera b)
+        {
+            return !(a == b);
+        }
+        public static implicit operator int(Tempera a)
+        {
+            return a._cantidad;
+        }
+        public static explicit operator String(Tempera a)
+        {
+            return a.Mostrar();
+        }
+        public static Tempera operator +(Tempera a, Tempera b)
+        {
+            if (a == b)
+                a._cantidad++;
+            return a;
+        }
+        public static Tempera operator +(Tempera a, int cantidad)
+        {
+            a._cantidad = a._cantidad + cantidad;
+            return a;
+        }
+
+        
+    }
+}
