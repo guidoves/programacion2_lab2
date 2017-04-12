9929b782fb5bf4fd457e70c0fbe79a482a856af9
guidoves <guido.vespignani@gmail.com>
2017-04-11 22:04:25 -0300

    commit

--- Vespignani.Guido/Clase7/Paleta.cs
+++ Vespignani.Guido/Clase7/Paleta.cs
@@ -8,8 +8,14 @@ namespace Clase7
 {
     public class Paleta
     {
+        #region Variables
+
         private Tempera[] _temperas;
         private int _cantMaxColores;
+
+        #endregion
+
+        #region Constructores
 
         private Paleta():this(5)
         {
@@ -20,6 +26,10 @@ namespace Clase7
             this._cantMaxColores = cantidad;
         }
 
+        #endregion
+
+
+
         private String mostrar()
         {
             String paleta = this._cantMaxColores.ToString();
@@ -29,6 +39,33 @@ namespace Clase7
                 paleta += " " + Tempera.mostrar(i);
             }
             return paleta;
+        }
+
+        private int obtenerIndice()
+        {
+            int retValue = -1;
+            for (int i = 0; i < this._temperas.Length; i++)
+            {
+                if (this._temperas[i] == null)
+                {
+                    retValue = i;
+                    break;
+                }
+            }
+            return retValue;
+        }
+        private int obtenerIndice(Tempera a)
+        {
+            int retValue = -1;
+            for (int i = 0; i < this._temperas.Length; i++)
+            {
+                if (this._temperas[i] == a)
+                {
+                    retValue = i;
+                    break;
+                }
+            }
+            return retValue;
         }
 
         public static implicit operator Paleta(int a)
@@ -42,12 +79,13 @@ namespace Clase7
         }
         public static Boolean operator ==(Paleta a, Tempera b)
         {
+            Boolean retValue = false;
             foreach (Tempera i in a._temperas)
             {
                 if (b == i)
-                    return true;
+                    retValue = true;
             }
-            return false;
+            return retValue;
         }
         public static Boolean operator !=(Paleta a, Tempera b)
         {
@@ -57,36 +95,59 @@ namespace Clase7
         {
             if (a == b)
             {
-                Tempera aux = Paleta.buscarTempera(a, b);
-                aux += b;
+                int aux = a.obtenerIndice(b);
+                if(aux >= 0)
+                a._temperas[aux] += b;
+                //Tempera aux = Paleta.buscarTempera(a, b);
+                //aux += b;
             }
             else
             {
-                Tempera aux = Paleta.buscarTempera(a, null);
-                aux = b;
+                int aux = a.obtenerIndice(b);
+                if(aux >= 0)
+                a._temperas[aux] = b;   
+                //Tempera aux = Paleta.buscarTempera(a, null);
+                //aux = b;
             }
             return a;
-        }           
-        
-        private static Tempera buscarTempera(Paleta a, Tempera b)
-        {
-            Tempera ret = null;
-            foreach (Tempera i in a._temperas)
-            {
-                if (b == i)
-                    ret = a._temperas[i];
-            }
-            return ret;
         }
+
+        //private static Tempera buscarTempera(Paleta a, Tempera b)
+        //{
+        //    Tempera ret = null;
+        //    foreach (Tempera i in a._temperas)
+        //    {
+        //        if (b == i)
+        //            ret = a._temperas[i];
+        //    }
+        //    return ret;
+        //}
         
         public static Paleta operator -(Paleta a, Tempera b)
         {
-            if(a == b)
+            if (a == b)
+            {
+                int aux = a.obtenerIndice(b);
+                int cantidadA = a._temperas[aux];
+                int cantidadB = b;
+                if ((cantidadA - cantidadB) <= 0)
+                    a._temperas[aux] = null;
+                else
+                {
+                }
+            }
+            return a;
         }
 
         public static Paleta operator +(Paleta a, Paleta b)
         {
-
+            Paleta c = new Paleta(a._cantMaxColores + b._cantMaxColores);
+            c._temperas = a._temperas;
+            foreach (Tempera i in b._temperas)
+            {
+                c += i;
+            }
+            return c;
         }
     }
 }
