f28a34394026a4935a17a02f270d64c6b80e5a99
guidoves <guido.vespignani@gmail.com>
2017-04-17 22:04:33 -0300

    commit

---
+++ Vespignani.Guido/Clase8/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Clase8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack a = new Stack();
+            Queue b = new Queue();
+            ArrayList c = new ArrayList();
+            Hashtable d = new Hashtable();
+
+            Tempera temp1 = new Tempera(ConsoleColor.Blue,"dsa",5);
+            Tempera temp2 = new Tempera(ConsoleColor.Cyan, "dsa", 8);
+            Tempera temp3 = new Tempera(ConsoleColor.DarkBlue,"asd",32);
+
+            d.Add(108,"hola");
+            d.Add(-99,"chau");
+            d.Add(33, "hola");
+
+
+            foreach (int i in d.Keys)
+            {
+                Console.WriteLine(d[i]);
+            }
+
+
+            //a.Push(temp1);
+            //a.Push(temp2);
+            //a.Push(temp3);
+
+
+            //int _cantidadA = a.Count;
+            //for(int i = 0 ; i < _cantidadA ; i++)
+            //{
+            //    Console.WriteLine();
+            //}
+
+            
+
+
+            c.Add(temp1);
+            c.Add(temp2);
+            c.Add(temp3);
+            //c.Sort();
+            
+            //foreach (Tempera i in c)
+            //{
+
+            //    Console.WriteLine(Tempera.mostrar(i));
+            //}
+            
+            
+
+            //a.Push(1);
+            //a.Push(2);
+            //a.Push(3);
+            //a.Push(4);
+            //a.Push(5);
+
+            //b.Enqueue(1);
+            //b.Enqueue(2);
+            //b.Enqueue(3);
+            //b.Enqueue(4);
+            //b.Enqueue(5);
+
+            //int _cantidad = a.Count, _cantidadB = b.Count;
+
+
+            //for (int i = 0; i < _cantidad; i++)
+            //{
+            //    int aux = (int)a.Pop();
+            //    Console.WriteLine(aux);
+            //    if (aux == 3)
+            //        break;
+            //}
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Vespignani.Guido/Clase8/Tempera.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase8
+{
+    public class Tempera
+    {
+        #region Variables de instancia
+
+        private ConsoleColor _color;
+        private String _marca;
+        private int _cantidad;
+
+        #endregion
+
+        #region Constructor
+
+        public Tempera(ConsoleColor color, String marca, int cantidad)
+        {
+            this._color = color;
+            this._marca = marca;
+            this._cantidad = cantidad;
+        }
+
+        #endregion
+
+
+        public static String mostrar(Tempera a)
+        {
+            return a.mostrar();
+        }
+
+        private String mostrar()
+        {
+            return this._marca + " " + this._color.ToString() + " " + this._cantidad.ToString();
+        }
+
+        public static Boolean operator ==(Tempera a, Tempera b)
+        {
+            if (a._color == b._color && a._marca == b._marca)
+                return true;
+            return false;
+        }
+        public static Boolean operator !=(Tempera a, Tempera b)
+        {
+            return !(a == b);
+        }
+        public static implicit operator int(Tempera a)
+        {
+            return a._cantidad;
+        }
+        public static explicit operator String(Tempera a)
+        {
+            return Tempera.mostrar(a);
+        }
+        public static Tempera operator +(Tempera a, Tempera b)
+        {
+            if (a == b)
+                a._cantidad += b._cantidad;
+            return a;
+        }
+        public static Tempera operator +(Tempera a, int cantidad)
+        {
+            a._cantidad =+ cantidad;
+            return a;
+        }
+    }
+}
