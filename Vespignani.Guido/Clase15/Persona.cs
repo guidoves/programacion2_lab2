c8343013be13885b69630da6a97e7b535f1f31df
guidoves <guido.vespignani@gmail.com>
2017-05-22 20:54:38 -0300

    commit

---
+++ Vespignani.Guido/Clase15/Persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase15
+{
+    public enum ESexo
+    {
+        Masculino,
+        Femenino,
+        Indefinido
+    }
+    public class Persona
+    {
+        protected string _nombre;
+        protected string _apellido;
+        protected int _edad;
+        protected ESexo _sexo;
+
+        public Persona(string nombre, string apellido, int edad, ESexo sexo)
+        {
+            this._apellido = apellido;
+            this._edad = edad;
+            this._nombre = nombre;
+            this._sexo = sexo;
+        }
+
+        public string GetNombre()
+        {
+            return this._nombre;
+        }
+        public string GetApellido()
+        {
+            return this._apellido;
+        }
+        public int GetEdad()
+        {
+            return this._edad;
+        }
+        public ESexo GetSexo()
+        {
+            return this._sexo;
+        }
+        public string ObtenerInfo()
+        {
+            StringBuilder info = new StringBuilder();
+            info.AppendLine("Nombre:" + this._nombre);
+            info.AppendLine("Apellido: " + this._apellido);
+            info.AppendLine("Edad: " + this._edad.ToString());
+            info.AppendLine("Sexo: " + this._sexo.ToString());
+            return info.ToString();
+        }
+    }
+}
---
+++ Vespignani.Guido/TestLibreria/Extensora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades.Externa.Sellada;
+
+namespace TestLibreria
+{
+    public static class Extensora
+    {
+        public static string ObtenerInfo(this PersonaExternaSellada persona )
+        {
+            StringBuilder a = new StringBuilder();
+            a.AppendLine(persona.Apellido);
+            a.AppendLine(persona.Edad.ToString());
+            a.AppendLine(persona.Sexo.ToString());
+            a.AppendLine(persona.Nombre);
+            return a.ToString();
+        }
+        public static int ObtenerCantidadDeLetras(this string str)
+        {
+            return str.Length;
+        }
+    }
+}
