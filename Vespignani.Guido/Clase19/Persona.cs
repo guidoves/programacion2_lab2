b018f9bf345995148e2138e336c70ebeb2c7d7be
guidoves <guido.vespignani@gmail.com>
2017-05-29 22:09:17 -0300

    commit

---
+++ Vespignani.Guido/Clase19/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase19
+{
+    [Serializable]
+    public class Persona
+    {
+        public string _nombre;
+        public int _edad;
+        public bool _esMayor;
+
+        public Persona(string nombre, int edad, bool esMayor)
+        {
+            this._esMayor = esMayor;
+            this._nombre = nombre;
+            this._edad = edad;
+        }
+        public Persona()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder persona = new StringBuilder();
+            persona.AppendLine("Nombre: " + this._nombre);
+            persona.AppendLine("Edad: " + this._edad);
+            persona.AppendLine("Es mayor de edad: " + this._esMayor.ToString());
+            return persona.ToString();
+        }
+    }
+}
--- Vespignani.Guido/TestLibreria/Program.cs
+++ Vespignani.Guido/TestLibreria/Program.cs
@@ -5,8 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Entidades.Externa;
 using Entidades.Externa.Sellada;
-
-
+using Clase19;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.Xml;
 namespace TestLibreria
 {
     class Program 
@@ -14,9 +17,81 @@ namespace TestLibreria
         
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Persona miPersona = new Persona("juan", 23, true);
+                Persona miPersona2 = new Persona("Pedro", 11, false);
+                Persona miPersona3 = new Persona("lucas", 24, true);
+                Persona miPersona4 = new Persona("malena", 30, true);
+                string path = AppDomain.CurrentDomain.BaseDirectory+"archivo.txt";
+                string path2 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+"archivo.txt";
+                string path3 = @"C:\Users\alumno\Desktop\archivo.dat";
+                string path4 = @"D:\arch.xml";
 
+                List<Persona> personas = new List<Persona>();
+
+                personas.Add(miPersona);
+                personas.Add(miPersona2);
+                personas.Add(miPersona3);
+                personas.Add(miPersona4);
+
+
+
+                XmlSerializer xmlSer = new XmlSerializer(typeof(List<Persona>));
+                XmlTextWriter xmlWri = new XmlTextWriter(path4, Encoding.UTF8);
+
+                xmlSer.Serialize(xmlWri, personas);
+                xmlWri.Close();
+
+                List<Persona> personas2 = new List<Persona>();
+
+                
+
+                //FileStream fileStream = new FileStream(path3, FileMode.Create);
+                //BinaryFormatter binary = new BinaryFormatter();
+                
+                
+
+                
+            
+           
+
+                //using (StreamWriter stream = new StreamWriter(path, true))
+                //{
+                //    stream.Write("El primer objeto es: ");
+                //    stream.WriteLine(miPersona.ToString());
+                //    stream.Write("El segundo objeto es: ");
+                //    stream.WriteLine(miPersona2.ToString());
+                //}
+                //using (StreamReader streamin = new StreamReader(path
+                //    ))
+                //{
+                //    Console.WriteLine(streamin.ReadToEnd());
+                //    //string a = streamin.ReadLine();
+                //    //while (a != null)
+                //    //{
+                //    //    Console.WriteLine(a);
+                //    //    a = streamin.ReadLine();
+                //    //    Console.ReadLine();
+                //    //}
+
+                //}
+
+                //binary.Serialize(fileStream, miPersona);
+                //fileStream.Close();
+                //Persona persona3 = (Persona)binary.Deserialize(new FileStream(path3,FileMode.Open));
+                //Console.WriteLine(persona3.ToString());
+                ////xmlSerializer.Serialize(xmlWriter, miPersona3);
+                ////xmlWriter.Close();
+                ////Persona persona4 = (Persona)xmlSerializer.Deserialize(new XmlTextReader(path4));
+                ////Console.WriteLine(persona4.ToString());  
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
              Console.ReadLine();
+            
            
         }
     }
